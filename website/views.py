fc31ca431b8e6de8b99b9d2929d5806fda86ed5a
Leonard Martin <116867789+lmartin2422@users.noreply.github.com>
2023-10-22 00:02:17 -0400

    Added comments to views.py

--- website/views.py
+++ website/views.py
@@ -1,10 +1,11 @@
 # views stores the standard routes for the website (any page user can visit)
 
-from flask import Blueprint
-from flask import render_template  # how to load a html template
-from flask import url_for
-from flask import request
-from flask import flash  # displays messages to the user
+from flask import Blueprint  # Importing the Blueprint class from Flask, used to organize your application into smaller components (ex: views, templates, and static files, among other things.)
+from flask import render_template  # Importing the render_template function, used to load an HTML template
+from flask import url_for  # Importing the url_for function, used to generate URLs for a specific function
+from flask import request  # Importing the request object, used to handle incoming requests
+from flask import flash  # Importing the flash function, used to display messages to the user
+
 
 views = Blueprint('views', __name__)  # sets up a blueprint for the flask application
 
